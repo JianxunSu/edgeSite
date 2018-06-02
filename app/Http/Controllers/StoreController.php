3af65736b41459fffcebee59377c225a922d707f
Kevin Su <jevin.su@yahoo.com>
2018-06-01 23:50:28 -0700

    init controller&models

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class StoreController extends Controller
+{
+    //TODO show list
+    public function showList()
+    {
+    }
+}
---
+++ app/Http/Controllers/SupervisorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SupervisorController extends Controller
+{
+    //TODO show list
+    public function showList()
+    {
+    }
+    //TODO host a store
+    public function hostStore()
+    {
+    }
+}
